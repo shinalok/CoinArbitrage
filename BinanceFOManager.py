2756a50e136dc3adfbf29907fa03cb7e3fbfa768
shinalok <57692148+shinalok@users.noreply.github.com>
2022-09-02 17:09:01 +0900

    바이낸스 거래 추가

--- BinanceFOManager.py
+++ BinanceFOManager.py
@@ -8,9 +8,13 @@ from APIManager import *
 import pandas as pd
 from multipledispatch import dispatch
 
-class BinanceManager(APIManager):
+from BinanceManager import BinanceManager
+
+
+class BinanceFOManager(BinanceManager):
+
     def __init__(self):
-        print("__load BinanceManager__")
+        print("__load BinanceFOManager__")
         # 업비트 키 로드
         with open("keys/binance_key.txt") as f:
             lines = f.readlines()
@@ -19,51 +23,48 @@ class BinanceManager(APIManager):
 
         self.api = ccxt.binance({
             'apiKey': access_key,
-            'secret': secret_key
+            'secret': secret_key,
+            'enableRateLimit': True,
+            'options': {
+                'defaultType': 'future'
+            }
+
         })
+        self.leverage = 1
 
-    def get_ohlcv(self, ticker):
-        ohlcvs = self.api.fetch_ohlcv(ticker, '1d')
-        df = pd.DataFrame(ohlcvs, columns=['date', 'open', 'high', 'low', 'close', 'volume'])
-        df['date'] = pd.to_datetime(df['date'], unit='ms')
-        df.set_index(keys='date', inplace=True)
-        return df
+    def set_leverage(self, leverage):
+        self.leverage = leverage
 
-    def get_balances(self):
-        bal = self.api.fetch_balance()
-        return bal['free']
+    def _set_leverage(self, ticker):
+        markets = self.api.load_markets()
+        market = self.api.market(ticker)
+        resp = self.api.fapiPrivate_post_leverage({
+            'symbol': market['id'],
+            'leverage': self.leverage
+        })
+        return resp
 
     def get_balance_amt(self, ticker):
         bal = self.api.fetch_balance()
+        positions = bal['info']['positions']
         conv_ticker = ticker.replace("/",'')
-        return bal[conv_ticker]
-
-    def get_orderbook(self, ticker):
-        orderbook = self.api.fetch_order_book(ticker)
-        orderbook_buys = orderbook['bids']
-        orderbook_sells = orderbook['asks']
-        sell = []
-        buy = []
-        for order in orderbook_buys:
-            buy.append({"price": order[0], "vol": order[1]})
-        for order in orderbook_sells:
-            sell.append({"price": order[0], "vol": order[1]})
-        return {"sell": sell, "buy": buy}
-
-    def get_current_price(self, ticker):
-        return self.api.fetch_ticker(ticker)['close']
+        res_bal = next((item for item in positions if item['symbol'] == conv_ticker), None)
+        return res_bal['positionAmt']
 
     @dispatch(str)
     def sell(self, ticker):
+        self._set_leverage(ticker)
         unit = self.get_balance_amt(ticker)
         return self.api.create_market_sell_order(ticker, unit)
 
     @dispatch(str, float)
     def sell(self, ticker, unit):
+        self._set_leverage(ticker)
         return self.api.create_market_sell_order(ticker, unit)  # unit: 수량
 
     @dispatch(str, float, float)
     def sell(self, ticker, price, unit):
+        self._set_leverage(ticker)
         return self.api.create_limit_sell_order(ticker, price, unit)
 
     @dispatch(str, float, int, int)
@@ -92,6 +93,7 @@ class BinanceManager(APIManager):
         if hasattr(self, 'count'):
             self.count += 1
             print(self.count)
+        self._set_leverage(ticker)
         return self.buy(ticker, market_price, unit)
 
     @dispatch(str, float)
@@ -101,17 +103,20 @@ class BinanceManager(APIManager):
         if hasattr(self, 'count'):
             self.count += 1
             print(self.count)
+        self._set_leverage(ticker)
         return self.api.create_market_buy_order(ticker, unit)  # unit: 수량. Upbit의 경우 금액이 들어감
 
     @dispatch(str, float, float)
     def buy(self, ticker, price, unit):
         print(ticker, price, unit)
+        self._set_leverage(ticker)
         return self.api.create_limit_buy_order(symbol=ticker, price=price, amount=unit)
 
     @dispatch(str, float, int, int)
     def buy(self, ticker, price, term, count):
         _div_price = (price / count)
         print(_div_price)
+        self._set_leverage(ticker)
         self.count = 0
         self.buy_target(ticker, _div_price)
         sched = BackgroundScheduler()
@@ -126,13 +131,14 @@ class BinanceManager(APIManager):
 
 
 if __name__ == '__main__':
-    api = BinanceManager()
+    api = BinanceFOManager()
     print(api.get_current_price("XRP/USDT"))
+    exit()
     order = api.get_orderbook("XRP/USDT")
     print(order)
     bal = api.get_balances()
     print(bal)
-    bal = api.get_balance_amt("XRP")
+    bal = api.get_balance_amt("XRP/USDT")
     print(bal)
     exit()
     df = api.get_ohlcv("XRP/USDT")
